8903f42eceea134bf04bd6d11c44405f736f3c53
🦉하늘 <sky@haneul-ui-MacBookPro.local>
2022-08-25 13:50:14 +0900

    feat: BoardPage 완성(쓰기 빼고)

--- src/components/Card/Memo.js
+++ src/components/Card/Memo.js
@@ -5,8 +5,9 @@ const colors = ['#FFDB8D', '#F390AF', '#9460A8', '#1DBCB8', '#F27478']
 
 const Container = styled.div`
     width: 200px;
-    height: 100%;
+    height: 120%;
     max-height: 150px;
+    min-height: 100px;
     background-color: ${props => colors[props.index]};
     /* background-color: #; */
     padding: 30px;
@@ -27,13 +28,11 @@ const Text = styled.p`
 export const Memo = (props) => {
     // 포스트잇 #FFDB8D #F390AF #9460A8 #1DBCB8 #F27478
     let index = Math.floor(Math.random() * 5);
-
-    
     const data = props.data;
 
     return (
         <Container index={index}>
-            <Text>벌레 잘 잡으시는 분 구합니다 제발 구해주세요 벌레 잘 잡으시는 분 구합니다 제발 구해주세요 덕성 세스콤을 기다립니다</Text>
+            <Text>{data.text}qjffqewfewfdsfdsodjifsjflksjlfkskdfjlksdjfksjflskdjf</Text>
         </Container>
     )
 }
--- src/pages/Home/BoardPage.js
+++ src/pages/Home/BoardPage.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../../components/Common/Menubar";
 import styled from "styled-components"
 import boardImage from "../../assets/image/bg-board.png"
 import { Memo } from "../../components/Card/Memo"
+import axios from "axios";
+import { useState } from "react";
 
 const PageContainer = styled.div`
   width: 100%;
@@ -19,21 +21,31 @@ const Board = styled.div`
   background-image: url(${boardImage});
   width: 100%;
   max-width: 1200px;
-  height: 500px;
+  /* height: 500px; */
   min-width: 400px;
   margin: 100px;
   padding: 30px;
+  height: 150%;
   flex-wrap: wrap;
 `
-const BoardPage = ({ history }) => {
 
+const BoardPage = ({ history }) => {
+  const [memos, setMemos] = useState([])
+  useEffect(() => {
+    axios.get("/api/memo/getMemos").then((response) => {
+      // console.log(response.data.memos)
+      setMemos(response.data.memos)
+    })
+  },[])
+  const MemoList = memos?.map((data, index) => {
+    return <Memo data={data} key={index} />
+  })
   return (
     <>
       <Header />
       <PageContainer>
         <Board>
-          <Memo></Memo>
-          <Memo></Memo>
+          {MemoList}
         </Board>
       </PageContainer>
     </>
