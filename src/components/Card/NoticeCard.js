7f3fa2d256862539c4802a9a440bc2473f7602e2
🦉하늘 <sky@haneul-ui-MacBookPro.local>
2022-08-26 09:16:30 +0900

    fix: 알림 쪽 예외처리

--- src/components/Card/NoticeCard.js
+++ src/components/Card/NoticeCard.js
@@ -15,14 +15,14 @@ const Text = styled.div`
 `
 export const NoticeCard = (props) => {
     const data = props.data;
-    console.log(data)
+    // console.log(data)
     // 닉네임 data.applicant.name
     // 타이틀 data.class.title
     // 신청 마감 여부
     return (
         <CardWrapper>
             {data &&
-            <Text>{data.applicant.name}님이 {data.class.title} 클래스를 신청하였습니다.</Text>
+            <Text>{data?.applicant?.name}님이 {data?.class?.title} 클래스를 신청하였습니다.</Text>
             }
         </CardWrapper>
     )
